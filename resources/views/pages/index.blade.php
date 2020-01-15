5ab2d9ade8c6ad4c91c038e8d18a37387755d7be
dagnyjones <dagny.jons5@gmail.com>
2020-01-15 05:58:46 +0100

    added some things

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>PLANTI-O</h1>
+        <p>The worlds biggest source of plant information, the best community of bloggers and everything plants!</p>
+        
+    </div>
+@endsection
